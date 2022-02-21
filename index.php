c34a1371db7fe71f4b41b7b061baab3db4dd1300
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-21 15:14:07 +0000

    created index.php styles.css script.js normilase.css .gitignore

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en-GB">
+
+<head>
+
+    <title>Piano Plinky Plonk</title>
+    <link href="normalize.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+
+</body>
+</html>
