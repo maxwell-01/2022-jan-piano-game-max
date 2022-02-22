7e7cf7f89c41831655c9b89cb533d60d8283a8db
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-22 15:49:34 +0000

    changed closeModel function to take a bool

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ let showInstructionsButton = document.querySelector('#instruction-button')
 let instructionsModal = document.querySelector('.instruction-modal')
 let hideInstructionsModal = document.querySelector('#modal-button')
 let closeModal = (status) => {
-    blurredAreaModal.style.display = status
-    instructionsModal.style.display = status
+    if(status === true) {
+        blurredAreaModal.style.display = 'none'
+        instructionsModal.style.display = 'none'
+    } else if( status === false) {
+        blurredAreaModal.style.display = 'block'
+        instructionsModal.style.display = 'block'
+    }
 }
 
 showInstructionsButton.addEventListener('click', (event) => {
     event.stopPropagation()
-    closeModal('block')
+    closeModal(false)
 })
 
 hideInstructionsModal.addEventListener('click', (event) => {
-    closeModal('none')
+    closeModal(true)
 })
 
 window.addEventListener('click', (event) => {
     if (event.target === blurredAreaModal && instructionsModal) {
         event.stopPropagation()
-        closeModal('none')
+        closeModal(true)
     }
 })
