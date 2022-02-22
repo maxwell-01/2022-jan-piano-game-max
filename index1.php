d69d8e807db5df33241846269a254fdc470faf47
Liam <liamlwrnce@gmail.com>
2022-02-22 14:09:40 +0000

    further CSS for game screen and parent page

--- index1.php
+++ index1.php
@@ -8,15 +8,17 @@
     <link href="normalize.css" rel="stylesheet" type="text/css">
     <link href="styles.css" rel="stylesheet" type="text/css">
     <script src="script.js" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
 
       <header><h1>Piano Plinky Plonk</h1></header>
   <main>
-      <div class="instruction-button-container">
-          <svg id="instruction-button" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm.5 17h-1v-9h1v9zm-.5-12c.466 0 .845.378.845.845 0 .466-.379.844-.845.844-.466 0-.845-.378-.845-.844 0-.467.379-.845.845-.845z"/></svg>
-      </div>
+
       <div class="modal-blurred-area is-hidden is-visuallyHidden">
           <div class="instruction-modal">
               <h2>Instructions</h2>
@@ -28,8 +30,8 @@
                       <li>using your keyboard, tap the corresponding keys to play notes</li>
                       <li class="remove-bullet-point">
                           <ul>
-                              <li> '⇪ ' (caps lock) to '\' are the white piano keys</li>
-                              <li>'Q' to ']' are the black piano keys</li>
+                              <li> 'A' to '↵' are the white piano keys</li>
+                              <li>'W' to ']' are the black piano keys</li>
                           </ul>
                       </li>
                   </ul>
@@ -38,6 +40,9 @@
       </div>
 
     <section class="game-container">
+        <div class="instruction-button-container">
+            <button id="instruction-button">&#8505</button>
+        </div>
              <div class="piano-key-container">
                   <div class="piano-key white-key" data-note="g0">
                       <div class="piano-key black-key" data-note="g0#">
@@ -111,7 +116,7 @@
 
   </main>
   <footer>
-    <a href="#">Meet the team!</a>
+    <a href="https://linktr.ee/charlotte.codes">Meet the team!</a>
   </footer>
 
 </body>
