6efb88b05d2b46d0909305f98f4b07af50a0f7af
Rich <richrar@gmail.com>
2022-02-24 09:57:50 +0000

    changes as requested in code review

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let showInstructionsButton = document.querySelector('#instruction-button')
-let hideInstructionsModal = document.querySelector('#modal-button')
-
 function openCloseModal(modalOverlay, modalBody, isModalOpen) {
     if(isModalOpen === false) {
         document.querySelector(modalOverlay).style.display = 'none'
@@ -10,6 +7,22 @@ function openCloseModal(modalOverlay, modalBody, isModalOpen) {
         document.querySelector(modalBody).style.display = 'block'
     }
 }
+
+function playNote(frequency) {
+    let sound = audioContext.createOscillator()
+    sound.connect(audioConnect)
+    sound.type = 'sine'
+    sound.frequency.value = frequency
+    sound.start()
+    return sound
+}
+
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+audioConnect = audioContext.createGain();
+audioConnect.connect(audioContext.destination);
+
+let showInstructionsButton = document.querySelector('#instruction-button')
+let hideInstructionsModal = document.querySelector('#modal-button')
 
 showInstructionsButton.addEventListener('click', (event) => {
     event.stopPropagation()
@@ -22,199 +35,197 @@ hideInstructionsModal.addEventListener('click', () => {
 
 let instructionsModal = document.querySelector('.instruction-modal')
 let blurredAreaModal = document.querySelector('.modal-blurred-area')
-window.addEventListener('click', (event) => {
-    if (event.target === blurredAreaModal && instructionsModal) {
+blurredAreaModal.addEventListener('click', (event) => {
+    if (event.target === blurredAreaModal) {
         event.stopPropagation()
         openCloseModal('.modal-blurred-area', '.instruction-modal', false)
     }
 })
 
-let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let notesPlaying = {};
-let audioConnect = null;
-let pianoKeys = document.querySelector('.piano-key-container')
+let pianoKeyContainer = document.querySelector('.piano-key-container')
 let keyBoardArray = [
     {
-        keyColour: 'white',
-        keyNote: 'g3',
-        keyCode: 'KeyA',
+        colour: 'white',
+        note: 'g3',
+        code: 'KeyA',
         label: 'A',
         pressed: false,
         frequency: 195.9977
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'g3',
-        keyNote: 'g3sharp',
-        keyCode: 'KeyW',
+        note: 'g3sharp',
+        code: 'KeyW',
         label: 'W',
         pressed: false,
         frequency: 207.6523
     },
     {
-        keyColour: 'white',
-        keyNote: 'a4',
-        keyCode: 'KeyS',
+        colour: 'white',
+        note: 'a4',
+        code: 'KeyS',
         label: 'S',
         pressed: false,
         frequency: 220.0000
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'a4',
-        keyNote: 'a4sharp',
-        keyCode: 'KeyE',
+        note: 'a4sharp',
+        code: 'KeyE',
         label: 'E',
         pressed: false,
         frequency: 233.0819
     },
     {
-        keyColour: 'white',
-        keyNote: 'b4',
-        keyCode: 'KeyD',
+        colour: 'white',
+        note: 'b4',
+        code: 'KeyD',
         label: 'D',
         pressed: false,
         frequency: 246.9417
     },
     {
-        keyColour: 'white',
-        keyNote: 'c4',
-        keyCode: 'KeyF',
+        colour: 'white',
+        note: 'c4',
+        code: 'KeyF',
         label: 'F',
         pressed: false,
         frequency: 261.6256
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'c4',
-        keyNote: 'c4sharp',
-        keyCode: 'KeyT',
+        note: 'c4sharp',
+        code: 'KeyT',
         label: 'T',
         pressed: false,
         frequency: 277.1826
     },
     {
-        keyColour: 'white',
-        keyNote: 'd4',
-        keyCode: 'KeyG',
+        colour: 'white',
+        note: 'd4',
+        code: 'KeyG',
         label: 'G',
         pressed: false,
         frequency: 293.6648
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'd4',
-        keyNote: 'd4sharp',
-        keyCode: 'KeyY',
+        note: 'd4sharp',
+        code: 'KeyY',
         label: 'Y',
         pressed: false,
         frequency: 311.1270
     },
     {
-        keyColour: 'white',
-        keyNote: 'e4',
-        keyCode: 'KeyH',
+        colour: 'white',
+        note: 'e4',
+        code: 'KeyH',
         label: 'H',
         pressed: false,
         frequency: 329.6276
     },
     {
-        keyColour: 'white',
-        keyNote: 'f4',
-        keyCode: 'KeyJ',
+        colour: 'white',
+        note: 'f4',
+        code: 'KeyJ',
         label: 'J',
         pressed: false,
         frequency: 349.2282,
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'f4',
-        keyNote: 'f4sharp',
-        keyCode: 'KeyI',
+        note: 'f4sharp',
+        code: 'KeyI',
         label: 'I',
         pressed: false,
         frequency: 369.9944
     },
     {
-        keyColour: 'white',
-        keyNote: 'g4',
-        keyCode: 'KeyK',
+        colour: 'white',
+        note: 'g4',
+        code: 'KeyK',
         label: 'K',
         pressed: false,
         frequency: 391.9954
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'g4',
-        keyNote: 'g4sharp',
-        keyCode: 'KeyO',
+        note: 'g4sharp',
+        code: 'KeyO',
         label: 'O',
         pressed: false,
         frequency: 415.3047
     },
     {
-        keyColour: 'white',
-        keyNote: 'a5',
-        keyCode: 'KeyL',
+        colour: 'white',
+        note: 'a5',
+        code: 'KeyL',
         label: 'L',
         pressed: false,
         frequency: 440.0000
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'a5',
-        keyNote: 'a5sharp',
-        keyCode: 'KeyP',
+        note: 'a5sharp',
+        code: 'KeyP',
         label: 'P',
         pressed: false,
         frequency: 466.1638
     },
     {
-        keyColour: 'white',
-        keyNote: 'b5',
-        keyCode: 'Semicolon',
+        colour: 'white',
+        note: 'b5',
+        code: 'Semicolon',
         label: ';',
         pressed: false,
         frequency: 493.8833
     },
     {
-        keyColour: 'white',
-        keyNote: 'c5',
-        keyCode: 'Quote',
+        colour: 'white',
+        note: 'c5',
+        code: 'Quote',
         label: "'",
         pressed: false,
         frequency: 523.2511
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'c5',
-        keyNote: 'c5sharp',
-        keyCode: 'BracketLeft',
+        note: 'c5sharp',
+        code: 'BracketLeft',
         label: '[',
         pressed: false,
         frequency: 554.3653
     },
     {
-        keyColour: 'white',
-        keyNote: 'd5',
-        keyCode: 'Backslash',
+        colour: 'white',
+        note: 'd5',
+        code: 'Backslash',
         label: '\\',
         pressed: false,
         frequency: 587.3295
     },
     {
-        keyColour: 'black',
+        colour: 'black',
         parentKey: 'd5',
-        keyNote: 'd5sharp',
-        keyCode: 'BracketRight',
+        note: 'd5sharp',
+        code: 'BracketRight',
         label: ']',
         pressed: false,
         frequency: 622.2540
     },
     {
-        keyColour: 'white',
-        keyNote: 'e5',
-        keyCode: 'Enter',
+        colour: 'white',
+        note: 'e5',
+        code: 'Enter',
         label: '⏎',
         pressed: false,
         frequency: 659.2551,
@@ -222,42 +233,30 @@ let keyBoardArray = [
 ]
 
 keyBoardArray.forEach((key) => {
-    if(key.keyColour === 'white') {
-        pianoKeys.innerHTML +=
-        '<div id="' + key.keyNote + '" class="piano-key white-key" data-id="' + key.keyNote + '">\n' +
+    if(key.colour === 'white') {
+        pianoKeyContainer.innerHTML +=
+        '<div id="' + key.note + '" class="piano-key white-key" data-id="' + key.note + '">\n' +
         '<p>' + key.label + '</p>\n' +
         '</div>'
-    } else if(key.keyColour === 'black') {
-        let parentWhiteKey = document.querySelector('#' + key.parentKey )
-        parentWhiteKey.innerHTML +=
-            '<div id="' + key.keyNote + '" class="piano-key black-key" data-note="' + key.keyNote + '">\n' +
+    } else if(key.colour === 'black') {
+        let parentWhiteKeyDiv = document.querySelector('#' + key.parentKey )
+        let newContent =
+            '<div id="' + key.note + '" class="piano-key black-key" data-note="' + key.note + '">\n' +
             '   <p>' + key.label + '</p>\n' +
-            '</div>\n'
+            '</div>\n' + parentWhiteKeyDiv.innerHTML
+        parentWhiteKeyDiv.innerHTML = newContent
     }
 })
-
-audioConnect = audioContext.createGain();
-audioConnect.connect(audioContext.destination);
-
-function playNote(frequency) {
-    let sound = audioContext.createOscillator()
-    sound.connect(audioConnect)
-    sound.type = 'sine'
-    sound.frequency.value = frequency
-    sound.start()
-    return sound
-}
 
 window.addEventListener('keydown' , (event) => {
     event.preventDefault()
     event.stopPropagation()
     keyBoardArray.forEach((key) => {
-        if(key.keyCode === event.code) {
-            if(key.pressed !== true) {
+        if(key.code === event.code) {
+            if(key.pressed === false) {
                 key.pressed = true
-                notesPlaying[key.keyNote] = playNote(key.frequency)
-                let noteClass = '#' + key.keyNote
-                let pianoKeyDiv = document.querySelector(noteClass)
+                notesPlaying[key.note] = playNote(key.frequency)
+                let pianoKeyDiv = document.querySelector('#' + key.note)
                 pianoKeyDiv.classList.add('depressedKey')
             }
         }
@@ -268,12 +267,11 @@ window.addEventListener('keyup' , (event) => {
     event.preventDefault()
     event.stopPropagation()
     keyBoardArray.forEach((key) => {
-        if(key.keyCode === event.code) {
-            if(key.pressed !== false) {
+        if(key.code === event.code) {
+            if(key.pressed === true) {
                 key.pressed = false
-                notesPlaying[key.keyNote].stop()
-                let noteClass = '#' + key.keyNote
-                let pianoKeyDiv = document.querySelector(noteClass)
+                notesPlaying[key.note].stop()
+                let pianoKeyDiv = document.querySelector('#' + key.note)
                 pianoKeyDiv.classList.remove('depressedKey')
             }
         }
